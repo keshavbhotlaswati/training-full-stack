6b5d23ceb3d8544b7e5c8edf9f70f9e56432dfb4
swati <swati.aug97@gmail.com>
2021-09-26 22:11:43 +0530

    Add files via upload

---
+++ JPADemo12/src/main/java/com/demo/Employee.java
@@ -0,0 +1,43 @@
+package com.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Employee {
+	@Id
+	private int id;	
+	private String name;
+	private String city;
+	private double salary;
+	
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
